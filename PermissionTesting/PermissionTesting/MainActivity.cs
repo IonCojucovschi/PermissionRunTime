07e4d95431ddc5d431ac38cefd4ade4f55657e30
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-03-05 18:41:46 +0200

    permission Run time

---
+++ PermissionTesting/PermissionTesting/MainActivity.cs
@@ -0,0 +1,114 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+using Android;
+using Android.Content.PM;
+using Android.Runtime;
+
+namespace PermissionTesting
+{
+    [Activity(Label = "PermissionTesting", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            await TryToGetPermissions();
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+
+
+
+        async Task TryToGetPermissions()
+        {
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                 GetPermissions();
+                return;
+            }
+
+
+        }
+
+        const int RequestLocationId = 0;
+
+        readonly string[] PermissionsGroupLocation =
+            {
+                            //TODO add more permissions
+            Manifest.Permission.AccessFineLocation,
+                            
+             };
+        async void GetPermissions()
+        {
+            const string permission = Manifest.Permission.AccessFineLocation;
+
+            if (CheckSelfPermission(permission) == (int)Permission.Granted)
+            {
+                //TODO change the message to show the permissions name
+                Toast.MakeText(this, "Camera permissions status: granted", ToastLength.Short).Show();
+                ///return;
+            }
+            var rez = ShouldShowRequestPermissionRationale(permission);
+            if (!ShouldShowRequestPermissionRationale(permission))
+            {
+                //set alert for executing the task
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Permissions Needed");
+                alert.SetMessage("The application need Camera permissions to continue");
+                alert.SetPositiveButton("Ok", (senderAlert, args) =>
+                {
+                    RequestPermissions(PermissionsGroupLocation, RequestLocationId);
+                });
+
+                alert.SetNegativeButton("Cancel", (senderAlert, args) =>
+                {
+                    Toast.MakeText(this, "Cancelled!", ToastLength.Short).Show();
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+
+
+                ///return;
+            }
+
+            RequestPermissions(PermissionsGroupLocation, RequestLocationId);
+
+        }
+        public override async void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            switch (requestCode)
+            {
+                case RequestLocationId:
+                    {
+                        if (grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+                        {
+                            Toast.MakeText(this, "Special permissions granted", ToastLength.Short).Show();
+
+                        }
+                        else
+                        {
+                            //Permission Denied :(
+                            Toast.MakeText(this, "Special permissions denied", ToastLength.Short).Show();
+
+                        }
+                    }
+                    break;
+            }
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+}
+
